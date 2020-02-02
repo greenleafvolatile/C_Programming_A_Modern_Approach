53e3818424ec9b10d302b6fb0d510bcf9359e908
Daan Pol <daanpol@tutamail.com>
2020-02-02 13:27:42 +0100

    Added Programming Project 1

---
+++ Ch_2/Exercises/Programming_Project_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+printf("       %s\n", "*");
+
+printf("      %s\n", "*");
+
+printf("     %s\n", "*");
+
+printf("%s   %s\n", "*", "*");
+
+printf(" %s %s\n", "*", "*");
+printf("  %s", "*");
+return 0;
+}
+
