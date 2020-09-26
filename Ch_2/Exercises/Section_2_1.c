00f72fbd31c709caec29e426ca29c533567878c6
greenleafvolatile <daanpol@tutamail.com>
2020-09-26 12:10:23 +0200

    Removed some files

--- Ch_2/Exercises/Section_2_1.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(void){
-printf("Hello, world\n"); // I control reaches the end of main without encountering a return statement, the effect is that of returning 0.
-}
-
