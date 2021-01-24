b46b0a1ff0e5430b01b1c6334a5a1f5013dfabd1
greenleafvolatile <daanpol@tutamail.com>
2021-01-24 15:04:18 +0100

    Added programming exercise 15.

--- Ch_12/Exercises/Programming_Exercise_15.c
+++ Ch_12/Exercises/Programming_Exercise_15.c
@@ -1,5 +1,7 @@
-/* "Write a loop that prints all temperature readings stored in row i of the temperatures 
-   array (see Exercise 14). Use a pointer to visit each element of the row." */
+/******************************************************************************************* 
+ *  "Write a loop that prints all temperature readings stored in row i of the temperatures *
+ *  array (see Exercise 14). Use a pointer to visit each element of the row."              * 
+ *******************************************************************************************/
 
 // Directives
 
